23098a62cad467e047c8048edf182c65d9214386
acorvin <48366916+acorvin@users.noreply.github.com>
2019-04-30 21:51:07 -0700

    additional comments

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // ANALOG CLOCK
 function setDate() {
   const now = new Date()
-
+  // calculate seconds
   const seconds = now.getSeconds()
   const secondsDegrees = (seconds / 60) * 360 + 90
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-
+  // calculate minutes
   const minutes = now.getMinutes()
   const minutesDegrees = (minutes / 60) * 360 + 90
   minutesHand.style.transform = `rotate(${minutesDegrees}deg)`
-
+  // calculate hours
   const hour = now.getHours()
   const hourDegrees = (hour / 12) * 360 + 90
   hourHand.style.transform = `rotate(${hourDegrees}deg)`
 
-  // Bezier curve glitch resolution
+  // Bezier curve transition glitch resolution 
   if (secondsDegrees === 90) {
     eachHand.forEach(hand => (hand.style.transition = 'none'))
   } else {
     eachHand.forEach(hand => (hand.style.transition = ''))
   }
 }
-
+// Get clock hands by class names
 const eachHand = document.querySelectorAll('.hand')
 const secondHand = document.querySelector('.second-hand')
 const minutesHand = document.querySelector('.minutes-hand')
@@ -30,8 +30,8 @@ const hourHand = document.querySelector('.hour-hand')
 setInterval(setDate, 1000)
 
 // DIGITAL CLOCK
-const audio = new Audio('audio/clip.wav')
 
+// Get current time
 function timeDisplay() {
   let date = new Date()
   let hh = date.getHours()
@@ -39,10 +39,12 @@ function timeDisplay() {
   let ss = date.getSeconds()
   let meridiem = ''
 
+  // assign values to display
   hh = hh < 10 ? '0' + hh : hh
   mm = mm < 10 ? '0' + mm : mm
   ss = ss < 10 ? '0' + ss : ss
 
+  // Set AM and PM
   if (hh === 0) {
     hh = 12
   }
@@ -52,29 +54,33 @@ function timeDisplay() {
   } else {
     meridiem = 'AM'
   }
-
+  // display current time from assigned values
   const time = `${hh}:${mm}:${ss} ${meridiem}`
   document.getElementById('digitalClock').textContent = time
 
+  //set ticking interval miliseconds
   setTimeout(timeDisplay, 1000)
 }
+// run the function
 timeDisplay()
 
-// CLOCK TOGGLE
+// Get analog and digital clock displays by id
 const displayAnalog = document.getElementById('clock')
 const displayDigital = document.getElementById('digitalClock')
 
+// Get button by id and listen for click event
 const btn = document.getElementById('btn')
 btn.addEventListener('click', toggleDisplay)
 
+// Toggle analog and digital clock displays
 function toggleDisplay() {
-  if (displayDigital.style.display === 'none') {
-    displayDigital.style.display = 'block'
-    displayAnalog.style.display = 'none'
-    btn.textContent = 'DIGITAL'
-  } else {
+  if (displayDigital.style.display === 'block') {
     displayDigital.style.display = 'none'
     displayAnalog.style.display = 'block'
+    btn.textContent = 'DIGITAL'
+  } else {
+    displayDigital.style.display = 'block'
+    displayAnalog.style.display = 'none'
     btn.textContent = 'ANALOG'
   }
 }
